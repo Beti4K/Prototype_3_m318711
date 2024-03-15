f21b4094c38b843c8be6b62d433e1d7d260c00c2
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-03-15 12:49:51 +0000

    SpawnManager upgrade
    Obstacle spawned at time intervals now

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,13 +6,15 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
-    void Update()
+    void SpawnObstacle()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
